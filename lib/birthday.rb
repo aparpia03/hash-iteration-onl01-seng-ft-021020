41b0f963870c81ccfc7973104d76eec904de36d4
Abbas Parpia <aparpia03@gmail.com>
2020-02-16 19:41:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ end
    birthday_kids.each do|name, age|
     if age <= 12
      return "Happy Birthday #{name}! You are now #{age} years old!"
-    elsif age > 12
+    else
       return "You are too old for this."
       end
     end 
